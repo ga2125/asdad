100ce33ea0f8d108055c1b667754043ca5e359a7
ga2125 <162793321+ga2125@users.noreply.github.com>
2024-03-09 20:42:11 +1100

    Delete script directory

--- script/files/utils/fromHex.lua
+++
@@ -1,7 +0,0 @@
-local function fromHex(str)
-    return (string.gsub(str, '..', function (cc)
-        return string.char(tonumber(cc, 16));
-    end));
-end;
-
-return fromHex;
--- script/files/utils/toCamelCase.lua
+++
@@ -1,5 +0,0 @@
-SX_VM_CNONE();
-local stringPattern = getServerConstant('%s(.)');
-return function (text)
-    return string.lower(text):gsub(stringPattern, string.upper);
-end;
